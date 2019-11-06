78d23e1ba663b8d677fd2f407b8ed5770f596076
hummingburd <anthony@hypar.io>
2019-11-06 15:57:04 -0500

    Restoration of earlier state

--- Envelope/EnvelopeBySketch/src/EnvelopeBySketch.cs
+++ Envelope/EnvelopeBySketch/src/EnvelopeBySketch.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System;
 using Elements;
 using Elements.Geometry;
 using GeometryEx;
@@ -16,12 +17,14 @@ namespace EnvelopeBySketch
         /// <returns>A EnvelopeBySketchOutputs instance containing computed results and the model with any new elements.</returns>
         public static EnvelopeBySketchOutputs Execute(Dictionary<string, Model> inputModels, EnvelopeBySketchInputs input)
         {
+            //var footprint = new Envelope(new Transform(), new Material("basement", Palette.Gray, 0.0f, 0.0f), new Representation(new List<Elements.Geometry.Solids.SolidOperation> { new Elements.Geometry.Solids.Extrude(input.Perimeter, input.BuildingHeight, Vector3.ZAxis, 0.0, false) }), Guid.NewGuid(), "");
+
             var footprint = new Mass(new Profile(input.Perimeter), input.FoundationDepth);
             if (footprint == null)
             {
                 footprint = new Elements.Mass(Polygon.Rectangle(50.0, 50.0), 0.1, BuiltInMaterials.Concrete);
             }
-            var basement = new Elements.Mass(footprint.Profile,
+            var basement = new Elements.Mass(input.Perimeter,
                                              input.FoundationDepth,
                                              new Material("basement", Palette.Gray, 0.0f, 0.0f),
                                              new Transform(footprint.Transform.Matrix));
