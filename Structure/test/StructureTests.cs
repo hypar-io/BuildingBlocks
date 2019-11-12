abdd02f3d92713ae7fb42cdbb0914c0209992e4f
Ian Keough <ian@hypar.io>
2019-11-12 12:37:13 -0800

    Update tests.

--- Structure/test/StructureTests.cs
+++ Structure/test/StructureTests.cs
@@ -16,19 +16,19 @@ namespace Structure.tests
         public void StructureTest()
         {
             var model = Model.FromJson(System.IO.File.ReadAllText("../../../../core.json"));
-            var inputs = new StructureInputs(4.0, 5.0, 0.5, "", "", new Dictionary<string, string>(), "", "", "");
+            var inputs = new StructureInputs(4.0, 5.0, 0.5, false, "", "", new Dictionary<string, string>(), "", "", "");
             var outputs = Structure.Execute(new Dictionary<string, Model>{{"envelope", model}}, inputs);
             System.IO.File.WriteAllText("../../../../structureCore.json", outputs.model.ToJson());
             outputs.model.ToGlTF("../../../../structureCore.glb");
 
             model = Model.FromJson(System.IO.File.ReadAllText("../../../../mass.json"));
-            inputs = new StructureInputs(4.0, 5.0, 0.5, "", "", new Dictionary<string, string>(), "", "", "");
+            inputs = new StructureInputs(4.0, 5.0, 0.5, false, "", "", new Dictionary<string, string>(), "", "", "");
             outputs = Structure.Execute(new Dictionary<string, Model>{{"envelope", model}}, inputs);
             System.IO.File.WriteAllText("../../../../structureMass.json", outputs.model.ToJson());
             outputs.model.ToGlTF("../../../../structureMass.glb");
 
             model = Model.FromJson(System.IO.File.ReadAllText("../../../../story.json"));
-            inputs = new StructureInputs(4.0, 5.0, 0.5, "", "", new Dictionary<string, string>(), "", "", "");
+            inputs = new StructureInputs(4.0, 5.0, 0.5, false, "", "", new Dictionary<string, string>(), "", "", "");
             outputs = Structure.Execute(new Dictionary<string, Model>{{"envelope", model}}, inputs);
             System.IO.File.WriteAllText("../../../../structureStory.json", outputs.model.ToJson());
             outputs.model.ToGlTF("../../../../structureStory.glb");
