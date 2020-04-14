5e4a46747d5ea02652e6a22d04f565bbd4a885d9
hummingburd <anthony@hypar.io>
2020-04-14 08:30:16 -0400

    Added EnvelopeByCenterline from private author repo
    - Andrew Heumann's addition

---
+++ Envelope/EnvelopeByCenterline/README.md
@@ -0,0 +1,21 @@
+<img src="preview.png" width="512">
+            
+# Envelope By Centerline
+
+Creates building Envelope elements derived from a polyline centerline sketch, width, and height.
+
+|Input Name|Type|Description|
+|---|---|---|
+|Centerline|Geometry|Centerline of the building envelope.|
+|Building Height|Range|Overall height of the building from grade.|
+|Bar Width|Range|Width of the mass perpendicular to the Centerline.|
+|Foundation Depth|Range|Depth of the building envelope below grade.|
+
+
+<br>
+
+|Output Name|Type|Description|
+|---|---|---|
+|Height|Number|Height of the building.|
+|Subgrade|Number|Depth of the subgrade section.|
+
---
+++ Envelope/EnvelopeByCenterline/src/EnvelopeByCenterline.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Elements;
+using Elements.Geometry;
+
+namespace EnvelopeByCenterline
+{
+    public static class EnvelopeByCenterline
+    {
+        /// <summary>
+        /// The EnvelopeByCenterline function.
+        /// </summary>
+        /// <param name="model">The input model.</param>
+        /// <param name="input">The arguments to the execution.</param>
+        /// <returns>A EnvelopeByCenterlineOutputs instance containing computed results and the model with any new elements.</returns>
+        public static EnvelopeByCenterlineOutputs Execute(Dictionary<string, Model> inputModels, EnvelopeByCenterlineInputs input)
+        {
+
+            var Centerline = input.Centerline;
+            var perimeter = Centerline.Offset(input.BarWidth / 2, EndType.Butt).First();
+
+            // Create the foundation Envelope.
+            var extrude = new Elements.Geometry.Solids.Extrude(perimeter, input.FoundationDepth, Vector3.ZAxis, false);
+            var geomRep = new Representation(new List<Elements.Geometry.Solids.SolidOperation>() { extrude });
+            var fndMatl = new Material("foundation", new Color(0.60000002384185791, 0.60000002384185791, 0.60000002384185791, 1), 0.0f, 0.0f);
+            var envMatl = new Material("envelope", new Color(0.30000001192092896, 0.699999988079071, 0.699999988079071, 0.6), 0.0f, 0.0f);
+            var envelopes = new List<Envelope>()
+            {
+                new Envelope(perimeter, input.FoundationDepth * -1, input.FoundationDepth, Vector3.ZAxis,
+                             0.0, new Transform(0.0, 0.0, input.FoundationDepth * -1), fndMatl, geomRep, false, Guid.NewGuid(), "")
+            };
+
+            // Create the Envelope at the location's zero plane.
+            var output = new EnvelopeByCenterlineOutputs(input.BuildingHeight, input.FoundationDepth);
+
+            extrude = new Elements.Geometry.Solids.Extrude(perimeter, input.BuildingHeight, Vector3.ZAxis, false);
+            geomRep = new Representation(new List<Elements.Geometry.Solids.SolidOperation>() { extrude });
+            envelopes.Add(new Envelope(perimeter, 0.0, input.BuildingHeight, Vector3.ZAxis, 0.0,
+                          new Transform(), envMatl, geomRep, false, Guid.NewGuid(), ""));
+            output.model.AddElements(envelopes);
+            var sketch = new Sketch(input.Centerline, Guid.NewGuid(), "Centerline Sketch");
+            output.model.AddElement(sketch);
+            return output;
+        }
+
+    }
+}
