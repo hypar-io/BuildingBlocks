7204a7dbb8d7fa8d8318fbce6dfbaa207b649dff
Gon Zifroni <gzifroni@gmail.com>
2022-08-06 19:10:00 -0700

    Updated the Grid function which was giving an error with run.

--- Grids/Grid/dependencies/GridNode.g.cs
+++ Grids/Grid/dependencies/GridNode.g.cs
@@ -10,6 +10,7 @@ using Elements.Geometry.Solids;
 using Elements.Spatial;
 using Elements.Validators;
 using Elements.Serialization.JSON;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,11 +22,11 @@ namespace Elements
     #pragma warning disable // Disable all warnings
 
     /// <summary>Represents a point at the intersection of two gridlines</summary>
-    [Newtonsoft.Json.JsonConverter(typeof(Elements.Serialization.JSON.JsonInheritanceConverter), "discriminator")]
+    [JsonConverter(typeof(Elements.Serialization.JSON.JsonInheritanceConverter), "discriminator")]
     [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "10.1.21.0 (Newtonsoft.Json v12.0.0.0)")]
     public partial class GridNode : Element
     {
-        [Newtonsoft.Json.JsonConstructor]
+        [JsonConstructor]
         public GridNode(Transform @location, string @uGridline, string @vGridline, System.Guid @id = default, string @name = null)
             : base(id, name)
         {
@@ -41,13 +42,13 @@ namespace Elements
         }
     
         /// <summary>The location of this grid node.</summary>
-        [Newtonsoft.Json.JsonProperty("Location", Required = Newtonsoft.Json.Required.DisallowNull, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        [JsonProperty("Location", Required = Newtonsoft.Json.Required.DisallowNull, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
         public Transform Location { get; set; }
     
-        [Newtonsoft.Json.JsonProperty("U Gridline", Required = Newtonsoft.Json.Required.DisallowNull, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        [JsonProperty("U Gridline", Required = Newtonsoft.Json.Required.DisallowNull, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
         public string UGridline { get; set; }
     
-        [Newtonsoft.Json.JsonProperty("V Gridline", Required = Newtonsoft.Json.Required.DisallowNull, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        [JsonProperty("V Gridline", Required = Newtonsoft.Json.Required.DisallowNull, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
         public string VGridline { get; set; }
     
     
