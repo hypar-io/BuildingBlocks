a80714fc83e97de20b6a8067f6a07418c36426fb
Anthonie Kramer <anthoniekramer@gmail.com>
2023-07-10 08:45:06 -0700

    added levelGroup output

--- Levels/LevelsFromFloors/src/LevelsFromFloors.cs
+++ Levels/LevelsFromFloors/src/LevelsFromFloors.cs
@@ -98,8 +98,13 @@ namespace LevelsFromFloors
             }
             var uniqueElevations = allFloors.Select(f => f.Elevation + f.Thickness).Distinct().OrderBy(l => l).ToList();
             var levelNameCounter = 1;
-            var levels = uniqueElevations.Select(l => new Level(l, Guid.NewGuid(), $"Level {levelNameCounter++}"));
+            var levels = uniqueElevations.Select(l => new Level(l, Guid.NewGuid(), $"Level {levelNameCounter++}")).ToList();
+
+            var levelGroup = new LevelGroup(levels, Guid.NewGuid(), "Levels From Floors", levels.Max(x => x.Elevation), default, "Levels From Floors Group");
+
             output.Model.AddElements(levels);
+            output.Model.AddElements(levelGroup);
+
             var levelPerimeters = allFloors.Select(f => new LevelPerimeter(f.Profile.Area(), f.Elevation, f.Profile.Perimeter, Guid.NewGuid(), f.Name));
             output.Model.AddElements(levelPerimeters);
             var floorsOrdered = allFloors.OrderBy(f => f.Elevation).ToList();
